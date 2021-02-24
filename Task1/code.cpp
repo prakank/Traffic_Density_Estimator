9d2c446a7c3195d3e6b9e79d3139e625f81e1357
johnnyblaze211 <64209825+johnnyblaze211@users.noreply.github.com>
2021-02-24 19:20:59 +0530

    Update code.cpp

--- Task1/code.cpp
+++ Task1/code.cpp
@@ -1,13 +1,16 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include<opencv2/opencv.hpp>
 #include <iostream>
 
 using namespace std;
 using namespace cv;
 
 Point2f src[4];
+// Point2f src_transformed[4];
 Point2f dst[4];
+// Point2f dst_transformed[4];
 int i = 0;
 float w = 0, h = 0; // Width and height of image
 
@@ -36,17 +39,18 @@ void onMouse(int event, int x, int y, int flags, void* params) {
 	}
 }
 
-int main() {
+int main(int argc, char * argv[]) {
 
-	// string path = "/home/prakank/opencv_test/learn_opencv/Resources/cards.jpg";
-	string path = "cards.jpg";
-	Mat img, imgWarp, matrix, img_temp;
+	string path = argv[1];
+	//string path = "empty.jpg";
+	Mat img, imgWarp, matrix, img_temp, img_transformed, img_cropped;
 	img = imread(path);
 	img_temp = imread(path);
 	if (img.empty()) {
 		cout << "Image not found or unable to load";
 		return -1;
 	}
+
 	cvtColor(img, img, COLOR_BGR2GRAY);
 	cvtColor(img_temp, img_temp, COLOR_BGR2GRAY);
 	imshow("Image", img);
@@ -55,41 +59,43 @@ int main() {
 	int j = 0;
 	while (i != 4) {
 		if (j != i) {
+
 			destroyWindow("Image");
-			circle(img, src[j], 10, Scalar(0, 0, 255), FILLED);
+			circle(img, src[j],10, Scalar(0, 0, 255), FILLED);
 			imshow("Image", img);
 			j = i;
-			if (i < 4)setMouseCallback("Image", onMouse, reinterpret_cast<void*>(&img));
+			if (i < 4)setMouseCallback("Image",onMouse, reinterpret_cast<void*>(&img));
 		}
 		waitKey(1);
 	}
 
 	if (i == 4) {
+
 		w = distance_new(src[0], src[1]) + distance_new(src[2], src[3]);
 		w /= 2;
 		h = distance_new(src[0], src[2]) + distance_new(src[1], src[3]);
 		h /= 2;
-		w *= 1.2;
-		h *= 1.5;
-		dst[0] = {0.0f, 0.0f};
-		dst[1] = {w, 0.0f};
-		dst[2] = {0.0f, h};
-		dst[3] = {w, h};
-		matrix = getPerspectiveTransform(src, dst);
-		warpPerspective(img_temp, imgWarp, matrix, Point(w, h));
 
-		for (int i = 0; i < 4; i++)
-		{
-			circle(img, src[i], 10, Scalar(0, 0, 255), FILLED);
-			// cout << src[i] << endl;
-		}
+		dst[0] = {472, 52};
+		dst[1] = {950, 52};
+		dst[2] = {472, 1030};
+		dst[3] = {950, 1030};
+		matrix = getPerspectiveTransform(src, dst);
+		warpPerspective(img_temp, imgWarp, matrix, img_temp.size());
+
+		Rect road(472,52, 478, 978);
+		img_cropped = imgWarp(road);
+
 		destroyWindow("Image");
-		imshow("Points Selected", img);
-		resize(imgWarp, imgWarp, Size(imgWarp.cols * 0.85, imgWarp.rows * 0.75));
-		// resizeWindow("Final Image", img.cols, img.rows * 0.8);
-		// namedWindow("Final Image", CV_WINDOW_AUTOSIZE);
-		imshow("Final Image", imgWarp);
+		// imshow("Points Selected", img);
+		imshow("Projected Image", imgWarp);
+		imwrite("Projected Image.jpg", imgWarp);
+		waitKey(0);
+		imshow("Cropped Image", img_cropped);
+		imwrite("Cropped Image.jpg", img_cropped);
 	}
+
 	waitKey(0);
 	return 0;
+
 }
